88291ba996fc457581388ccb8df0f56db3f3ee77
didimango <53489397+didimango@users.noreply.github.com>
2021-01-29 11:51:07 +0200

    added routing, added header, added 4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeContainerComponent } from './home-container/home-container.component';
+import { CategoryContainerComponent } from './category-container/category-container.component';
+import { ProductContainerComponent } from './product-container/product-container.component';
+import { CartContainerComponent } from './cart-container/cart-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeContainerComponent,
+    CategoryContainerComponent,
+    ProductContainerComponent,
+    CartContainerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
