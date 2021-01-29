d0c87c7cd271cc5cbfee982c44ca2a2ae6955295
jude2go2 <73139924+jude2go2@users.noreply.github.com>
2021-01-29 13:39:04 +0200

    Revert "added routing, added header, added 4 components"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartContainerComponent } from './cart-container/cart-container.component';
-import { CategoryContainerComponent } from './category-container/category-container.component';
-import { HomeContainerComponent } from './home-container/home-container.component';
-import { ProductContainerComponent } from './product-container/product-container.component';
 
-const routes: Routes = [
-  { path: '', component: HomeContainerComponent },
-  { path: 'category/:id', component: CategoryContainerComponent },
-  { path: 'product/:id', component: ProductContainerComponent },
-  { path: 'cart', component: CartContainerComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeContainerComponent } from './home-container/home-container.component';
-import { CategoryContainerComponent } from './category-container/category-container.component';
-import { ProductContainerComponent } from './product-container/product-container.component';
-import { CartContainerComponent } from './cart-container/cart-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeContainerComponent,
-    CategoryContainerComponent,
-    ProductContainerComponent,
-    CartContainerComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
