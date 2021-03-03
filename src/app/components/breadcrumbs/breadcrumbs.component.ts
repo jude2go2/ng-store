9b2dfcdde08bfbc054dd7425cdfdd0254ad1b7fd
didimango <53489397+didimango@users.noreply.github.com>
2021-03-03 11:46:13 +0200

    finished containers

---
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IBreadcrumb } from '../../models';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.scss'],
+})
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
+  constructor(private router: Router) {}
+
+  public breadcrumbs: Array<IBreadcrumb> = [];
+  private subscription: Subscription = new Subscription();
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.router.events.subscribe((data) => {
+        console.log(data);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
