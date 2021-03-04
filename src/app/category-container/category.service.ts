c55720e83c1d0943f6a2691837b1979aa0c367f1
didimango <53489397+didimango@users.noreply.github.com>
2021-03-04 12:51:48 +0200

    category routing

--- src/app/category-container/category.service.ts
+++ src/app/category-container/category.service.ts
@@ -6,31 +6,38 @@ import { ICategory } from './models';
 })
 export class CategoryService {
 
+  public categories: Array<ICategory> = [
+    {
+      id:1,
+      name: "Women"
+    },
+    {
+      id:2,
+      name: "Men"
+    },
+    {
+      id:3,
+      name: "Boys"
+    },
+    {
+      id:4,
+      name: "Girls"
+    },
+    {
+      id:5,
+      name: "Shoes"
+    },
+  ]
+
   constructor() { }
 
   public getCategories(): Array<ICategory> {
-    return  [
-      {
-        id:1,
-        name: "Women"
-      },
-      {
-        id:2,
-        name: "Men"
-      },
-      {
-        id:3,
-        name: "Boys"
-      },
-      {
-        id:4,
-        name: "Girls"
-      },
-      {
-        id:5,
-        name: "Shoes"
-      },
-    ]
+    return  this.categories
+  }
+
+  public getCategory(id: string): ICategory {
+    const numberId = parseInt(id)
+    return this.categories.find(category => category.id === numberId);
   }
 
 }
